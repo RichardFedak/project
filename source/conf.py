b4d348eefbd9a546f7fda2b92beed8dfd5e214b7
RichardFedak <104583219+RichardFedak@users.noreply.github.com>
2024-11-26 13:46:23 +0100

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -14,5 +14,5 @@ templates_path = ['_templates']
 exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-html_theme = 'press'
+html_theme = 'conestack'
 html_static_path = ['_static']
