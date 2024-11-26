4cb5c65e2d5d9eba22321a476c06b13323810970
RichardFedak <104583219+RichardFedak@users.noreply.github.com>
2024-11-26 13:50:45 +0100

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -14,5 +14,5 @@ templates_path = ['_templates']
 exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-html_theme = 'book'
+html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
